d0f5274c58183ece35fca3d772b5f43da21539e0
Vishal Gaur <34275219+SwordSaintLancelot@users.noreply.github.com>
2019-07-24 15:30:26 +0300

    basic model

--- 2-D_Linear_Model.py
+++ 2-D_Linear_Model.py
@@ -4,4 +4,24 @@ Created on Mon Jul 22 13:16:08 2019
 
 @author: GHOST
 """
+import numpy as np
+import matplotlib.pyplot as plt
 
+def linfit(x,y):
+    n = len(x)
+    a = (sum(x*y)-1/n*sum(x)*sum(y))/ ((sum(x**2))-1/n*(sum(x)**2))
+    b = sum(y)/n-sum(x)/n*a
+    return a,b
+
+
+if __name__ =="__main__":
+    n = int(input ('number of coordinates: '))
+    x,y = [],[]
+    for i in range(n):
+        x.append( int(input("enter the x coordinates: ")))
+        y.append(int( input("enter the y coordinates: ")))
+    a,b = linfit(x,y)
+    nx = [np.arange(-20,2,50)]
+    ny = a*nx+b
+    plt.scatter(nx,ny)
+    plt.show()

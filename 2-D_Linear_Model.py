cf8b53f78299b956a6fbf3d4a42a2c692e329920
Vishal Gaur <34275219+SwordSaintLancelot@users.noreply.github.com>
2019-07-25 15:41:10 +0300

    2-D linear model for coordinates x,y

--- 2-D_Linear_Model.py
+++ 2-D_Linear_Model.py
@@ -8,21 +8,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def linfit(x,y):
-    n = len(x)
-    for i in range(n):
-        a = (sum(x[i]*y[i])-1/n*sum(x[i])*sum(y[i]))/ ((sum(x[i]**2))-1/n*(sum(x[i])**2))
-        b = sum(y[i])/n-sum(x[i])/n*a
+    n = len(x)                  # length of one array
+    x1 = np.mean(x)                 # calculate mean for both the vectors to simplify the equations
+    y1 = np.mean(y)
+    a = (np.sum(x1*y1)-n*x1*y1)/(np.sum(x1**2)-(n*x1*x1))       #Linear Regression Equation. To be derivated using Least Square method
+    b = y1-a*x1
     return a,b
 
 
-if __name__ =="__main__":
-    n = int(input ('number of coordinates: '))
-    x,y = [],[]
-    for i in range(n):
-        x.append( int(input("enter the x coordinates: ")))
-        y.append(int( input("enter the y coordinates: ")))
-    a,b = linfit(x,y)
-    nx = [np.arange(-20,2,50)]
-    ny = a*nx+b
-    plt.scatter(nx,ny)
+def plotting(x,y,a,b):
+    plt.scatter(x,y,marker = "x", color = 'red')
+    # Plot the first scatter graph with initial values of arrays provided
+    pred_val = b+a*x
+    #predict the values
+    plt.plot(x,pred_val, marker = "o", color="blue")
+    #plot the predicted linear regression line
     plt.show()
+    
+
+if __name__ =="__main__":
+    #n = int(input ('number of coordinates: '))
+    x = np.array([0,1,2,3,4,5])
+    y = np.array([0,1,2,3,4,5])
+    a,b = linfit(x,y)
+    plotting(x,y,a,b)
